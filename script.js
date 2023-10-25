533ecc4a3bbed2002dadc2c998099579892c9b7d
MontagneIhsane <ihsane.montagne@gmail.com>
2023-10-25 13:16:21 +0200

    cinquième commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// function lampe(){
+// function changeImage(){
 //     const image = document.getElementById('myImage');
 //     let etatLampe = 0;
 //     //newImageSource  = newImageSource === "Logo.png"? "bg_img.jpg" : "Logo.png";
-//     if(newImageSource === "Logo.png"){
-//         newImageSource = "ampoule-led-e27-a-filaments-800-lumens.jpg"
+//     if(newImageSource === "img/bulb_PNG1254.png"){
+//         newImageSource = "img/ampoule-led-e27-a-filaments-800-lumens.jpg"
 //         etatLampe = 1;
 //     }else{
-//         newImageSource =  "bulb_PNG1254.png"
+//         newImageSource =  "img/bulb_PNG1254.png"
 //         etatLampe = 0;
 //     }
 //     image.src = newImageSource;
@@ -15,22 +15,36 @@
 
 function changeImage() {
     var image = document.getElementById('myImage');
+    let etatLampe = 0;
     if (image.src.match('img/ampoule-led-e27-a-filaments-800-lumens.jpg')) {
         image.src = 'img/bulb_PNG1254.png';
+        etatLampe = 1;
     } else {
         image.src = 'img/ampoule-led-e27-a-filaments-800-lumens.jpg';
+        etatLampe = 0;
     }
+    fetch("http://localhost:8080/lampe?on="+etatLampe);
 } 
 
 function changeImage_2() {
     var image = document.getElementById('myImage_2');
+    let etatgaz = 0;
     if (image.src.match('img/Comment-couper-une-arrivee-de-gaz-maisonbonte.png')) {
         image.src = 'img/gettyimages-1301820392-612x612.jpg';
+        etatgaz = 1;
     } else {
         image.src = 'img/Comment-couper-une-arrivee-de-gaz-maisonbonte.png';
+        etatLampe = 0;
     }
+    fetch("http://localhost:8080/gaz?on="+etatgaz);
 }
-function adjustOpacity(value) {
-    var image = document.getElementById('myImage_3');
 
-}
+const imageElement = document.getElementById('slider-image');
+const slider = document.getElementById('image-slider');
+
+const images = ["img/porte-fermee-blanche-isolee_1284-43080.avif", "img/entre ouverte.webp", "img/porte ouverte.webp"];
+
+slider.addEventListener('input', function() {
+    const index = slider.value - 1;
+    imageElement.src = images[index];
+});
