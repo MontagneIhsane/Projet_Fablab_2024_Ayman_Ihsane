a35673ce3823134c35fd58ecb6a6587a57957030
MontagneIhsane <ihsane.montagne@gmail.com>
2023-10-26 09:54:44 +0200

    end

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// function changeImage(){
-//     const image = document.getElementById('myImage');
-//     let etatLampe = 0;
-//     //newImageSource  = newImageSource === "Logo.png"? "bg_img.jpg" : "Logo.png";
-//     if(newImageSource === "img/bulb_PNG1254.png"){
-//         newImageSource = "img/ampoule-led-e27-a-filaments-800-lumens.jpg"
-//         etatLampe = 1;
-//     }else{
-//         newImageSource =  "img/bulb_PNG1254.png"
-//         etatLampe = 0;
-//     }
-//     image.src = newImageSource;
-//     fetch("http://localhost:8080/lampe?on="+etatLampe);
-// }
-
-function changeImage() {
+async function changeImage() {
     var image = document.getElementById('myImage');
     let etatLampe = 0;
     if (image.src.match('img/L1.JPG')) {
@@ -23,10 +8,10 @@ function changeImage() {
         image.src = 'img/L1.JPG';
         etatLampe = 0;
     }
-    fetch("http://localhost:8080/lampe?on="+etatLampe);
+    const response_2 = await fetch("http://localhost:8080/lampe?on="+etatLampe);
 } 
 
-function changeImage_2() {
+async function changeImage_2() {
     var image = document.getElementById('myImage_2');
     let etatgaz = 0;
     if (image.src.match('img/R1.JPG')) {
@@ -36,10 +21,10 @@ function changeImage_2() {
         image.src = 'img/R1.JPG';
         etatLampe = 0;
     }
-    fetch("http://localhost:8080/gaz?on="+etatgaz);
+    const response_2 = await fetch("http://localhost:8080/gaz?on="+etatgaz);
 }
 
-function changeImage_3() {
+async function changeImage_3() {
     var image = document.getElementById('myImage_5');
     let etatinperphone = 0;
     if (image.src.match('img/interphone_coupe.png')) {
@@ -49,7 +34,7 @@ function changeImage_3() {
         image.src = 'img/interphone_coupe.png';
         etatinperphone = 0;
     }
-    fetch("http://localhost:8080/interphone?on="+etatinperphone);
+    const response_3 = await fetch("http://localhost:8080/interphone?on="+etatinperphone);
 }
 
 const imageElement = document.getElementById('slider-image');
@@ -57,11 +42,12 @@ const slider = document.getElementById('image-slider');
 
 const images = ["img/P1.JPG", "img/P2.JPG", "img/P3.JPG"];
 
-slider.addEventListener('input', function() {
+slider.addEventListener('input', async function() {
     const index = slider.value - 1;
     imageElement.src = images[index];
+    const response_4 = await fetch("http://localhost:8080/door?paramas="+ slider.value);
 });
-const reponse_1 = fetch("http://localhost:8080/door?paramas="+ slider.value);
+
 
 
 const imageElement_4 = document.getElementById('slider-image_2');
@@ -69,8 +55,9 @@ const slider_4 = document.getElementById('image-slider_2');
 
 const images_4 = ["img/F1.JPG", "img/F2.JPG", "img/F3.JPG"];
 
-slider_4.addEventListener('input', function() {
+slider_4.addEventListener('input', async function() {
     const index_1 = slider_4.value - 1;
     imageElement_4.src = images_4[index_1];
+    const response_5 = await fetch("http://localhost:8080/fenetre?paramas="+slider_4.value);
 });
-const reponse_2 = fetch("http://localhost:8080/fenetre?paramas="+slider_4.value);
+
